fa470e346ac14aa917f90b333fffed3aa2e848f1
shawn446 <shawn@rivvet.io>
2025-11-24 14:40:48 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+
+  async rewrites() {
+    return [
+      {
+        // Allow URLs like /api/ring/80.png
+        source: "/api/ring/:score.png",
+        destination: "/api/ring/:score",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
