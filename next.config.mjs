1a21295d8409edda43b13cd5333d87dc823c5fb7
shawn446 <shawn@rivvet.io>
2025-11-24 15:06:05 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,18 @@ const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
 
+  // REGISTER WASM AS A STATIC FILE TYPE
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: "asset/resource",
+    });
+    return config;
+  },
+
   async rewrites() {
     return [
       {
-        // Allow URLs like /api/ring/80.png
         source: "/api/ring/:score.png",
         destination: "/api/ring/:score",
       },
